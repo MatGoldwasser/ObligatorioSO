ae7d826ec74f6becb6755efa05d2465f97b51f84
Nahuel <nahuelsaad2002@gmail.com>
2022-10-26 10:57:27 -0300

    Avanzamos en la validacion matricula y cedula, falta la validacion fecha

--- menu.sh
+++ menu.sh
@@ -2,28 +2,33 @@ registrarMatricula(){
     #si el usuario entra a esta opcion sin permisos de escritura se debe notificar que la operación no será guardada
     echo "Ingrese la matricula"
     read matricula
-    #https://es.stackoverflow.com/questions/255732/ayuda-con-expresion-regular-para-validar-matriculas-de-coche
-    if [ $matricula = ^[ABCDEFGHIJKLMNOPQRSTUVWXYZ]{3}-[0-9]{4} ]; then
-        echo "La matricula es valida"
+    echo "mat:$matricula"
+    if [[ "$matricula" =~ ^[S]{1}[A-Z]{2}(-)[0-9]{4}$ ]]; then
         echo "Ingrese la cédula del responsable"
         read cedula
-        if [ValidarCedula 'cedula' ]; then
-            echo "La cedula es valida"
+        if [[ "$cedula" =~ ^[0-9]{1}(.)[0-9]{3}(.)[0-9]{3}(-)[0-9]{1}$ ]]; then
+            echo "La cedula es válida"
             echo "Ingrese fecha de vencimiento (YYYY-MM-DD)"
             read fechaVenc
-            echo "$matricula | $cedula | $fechaVenc" >>matriculas.txt
-            echo "Operacion exitosa"
+            if [ "'date '+%Y-%m-%d' -d $fechaVen 2>/dev/null'" = "$fechaVen" ]; then
+                echo "$matricula | $cedula | $fechaVenc" >> matriculas.txt
+                echo "Operacion exitosa"
+            else   
+                echo "fecha inválida"
+            fi
 
-        else 
-            echo "Cedula invalida"
+        else
+            echo "Cedula inválida"
             exit
         fi
     else
-        echo "La matricula no es valida"
+        echo "La matricula no es válida"
         exit
     fi
     menuInicio
 }
+
+#[ "$fechaVen" =~ ^[2]{1}[0]{1}[0-9]{2}(-)((0)[1-9]|(1)[0-2])((0)[1-9]|[1-2][0-9]|3[0-1])$ ]
 
 verMatriculasRegistradas(){
     #if [fechaVencimiento < fechaActual]
@@ -80,30 +85,33 @@ cambiarPermisoModificacion(){
 }
 
 menuInicio(){
-    echo "Seguros ConductORT"
+    opcion=1
+    while [ $opcion != 5 ]; do
+        echo "Seguros ConductORT"
 
-    echo "1) Registrar Matricula"
-    echo "2) Ver Matriculas Registradas"
-    echo "3) Buscar Matriculas por Usuario"
-    echo "4) Cambiar Permiso de Modificacion"
-    echo "5) Salir"
+        echo "1) Registrar Matricula"
+        echo "2) Ver Matriculas Registradas"
+        echo "3) Buscar Matriculas por Usuario"
+        echo "4) Cambiar Permiso de Modificacion"
+        echo "5) Salir"
 
-    read opcion
-    if [ $opcion = 1 ]; then
-        registrarMatricula
-    elif [ $opcion = 2 ]; then
-        verMatriculasRegistradas
-    elif [ $opcion = 3 ]; then 
-        buscarMatriculasPorUsuario
-    elif [ $opcion = 4 ]; then
-        cambiarPermisoModificacion
-    elif [ $opcion = 5 ]; then
-        echo "Saliendo..."
-        exit
-    else
-        echo "No es una opcion no valida"
-    fi
-
+        read opcion
+        if [ $opcion = 1 ]; then
+            registrarMatricula
+        elif [ $opcion = 2 ]; then
+            verMatriculasRegistradas
+        elif [ $opcion = 3 ]; then 
+            buscarMatriculasPorUsuario
+        elif [ $opcion = 4 ]; then
+            cambiarPermisoModificacion
+        elif [ $opcion = 5 ]; then
+            echo "Saliendo..."
+            exit
+        else
+            echo "No es una opcion no valida"
+        fi
+    done
+    echo "Saliendo..."
 }
 
 menuInicio
